bef4b3b51712018fd309a292dea1a7a476f4f992
DarkDestiny26 <julianww@gmail.com>
2024-12-15 23:32:01 +0800

    Added login and register user

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:julianwong7828@localhost:5432/testdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    from models import User
+
+    @login_manager.user_loader
+    def load_user(uid):
+        return User.query.get(uid)
+    
+    bcrypt = Bcrypt(app)
      
     from routes import register_routes
-    register_routes(app)
+    register_routes(app, db, bcrypt)
 
     migrate = Migrate(app, db)
 
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def __repr__(self):
-        return f"username={self.username}" 
+        return f"username={self.username}"
+    
+    def get_id(self):
+        return self.id
