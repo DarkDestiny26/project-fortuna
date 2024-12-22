714223a37b917ab0215f936c23a4ebd201eb4c70
DarkDestiny26 <julianww@gmail.com>
2024-12-22 12:54:18 +0800

    added base template for website header

--- myapp/blueprints/home/routes.py
+++ myapp/blueprints/home/routes.py
@@ -4,9 +4,14 @@ from flask_login import login_user, logout_user, current_user, login_required
 from myapp.blueprints.auth.models import User
 from myapp.app import db
 
-home = Blueprint('home', __name__, template_folder='templates')
+home = Blueprint('home', __name__, template_folder='templates', static_folder='static')
 
 @home.route('/')
 @login_required
 def homepage():
     return render_template('home/home.html', username=current_user.username)
+
+@home.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('home/dashboard.html')
