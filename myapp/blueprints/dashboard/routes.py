efdcd013999c41e27049237b9a3c6643faf6faa9
DarkDestiny26 <julianww@gmail.com>
2025-02-08 02:50:08 +0800

    redesigned dashboard

--- myapp/blueprints/dashboard/routes.py
+++ myapp/blueprints/dashboard/routes.py
@@ -10,3 +10,8 @@ dashboard = Blueprint('dashboard', __name__, template_folder='templates', static
 @login_required
 def index():
     return render_template('dashboard/dashboard.html', username = current_user.username)
+
+@dashboard.route('/test')
+@login_required
+def index1():
+    return render_template('dashboard/dashboard2.html', username = current_user.username)
